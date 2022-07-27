a8748d8fdc9a557ee8853c072703b4daa42ad139
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-07-27 19:53:18 +0900

    Add OpenMPI build procedure

--- README.md
+++ README.md
@@ -6,10 +6,23 @@
 
 ビルドにあたっては以下のコマンド、ツールがセットアップされていることを前提とします
 
+- Environment Modules(必須ではないが推奨)
+  - セットアップされていない場合はPATHをスクリプト内で自動的に解決しますが、DIRACなどの実行時に毎回OpenMPIのパスを設定しないといけなくなるのでmoduleコマンドを使える状態にすることを推奨します  
+  - [公式サイトはこちら](http://modules.sourceforge.net/)  
+  - 他のPre-requirementsがEnvironment Modulesを使ってセットアップされている場合、スクリプト内でmodule purgeをしているため、setup.shのmodule purgeのあとでmodule loadをするようにスクリプトを変更してから実行してください  
+  (例: Intel(R) FortranやMKLをEnvironment Modulesを使ってintel-fortran及びmklという名前でセットアップしている場合)  
+
+  ```sh
+    # Clean modulefiles
+    module purge
+    module load intel-fortran
+    module load mkl
+  ```
+
 - MOLCAS
   - license.dat(ライセンスファイル)が./molcasディレクトリもしくは$HOME/.Molcasディレクトリ直下に配置されていること
-- Intel(R) Fortran compiler, Math kernel library
-  - [Intel(R) Fortran compiler](https://www.intel.com/content/www/us/en/developer/tools/oneapi/toolkits.html)
+- Intel(R) Fortran, C, C++ compiler, Math kernel library
+  - [Intel(R) Fortran, C, C++ compiler](https://www.intel.com/content/www/us/en/developer/tools/oneapi/toolkits.html)
   - [Intel(R) Math kernel library](https://www.intel.com/content/www/us/en/develop/documentation/get-started-with-mkl-for-dpcpp/top.html)
   - 上記2つはsudo権限及びインターネットへの接続が可能なら以下のコマンドでインストールできます
 
@@ -26,8 +39,12 @@
 
 ## How to setup
 
-以下のコマンドを実行するとビルドが実行されます
+以下のコマンドを実行するとビルドが実行されます  
+(SETUP_NPROCSの値は6以上を推奨します)  
+(SETUP_NPROCSの値が不正であるか、設定されていない場合SETUP_NPROCSの値は1になります)
 
 ```sh
- sh setup.sh
+ SETUP_NPROCS=使用コア数 sh setup.sh
+ # (e.g.)
+ SETUP_NPROCS=8 sh setup.sh
 ```
--- setup.sh
+++ setup.sh
@@ -13,8 +13,9 @@ function setup_git () {
 
 
 function setup_cmake () {
-	# unzip cmake tarball(prebuild)
+	# unzip cmake prebuild tarball
 	tar -xf "./cmake/cmake-${CMAKE_VERSION}-linux-x86_64.tar.gz" -C "${CMAKE}"
+	# ${CMAKE} にコピーされたtarballは使わないが、あとからどのtarballを使ってビルドしたか確認しやすくするためにコピーしておく
 	cp "./cmake/cmake-${CMAKE_VERSION}-linux-x86_64.tar.gz" "${CMAKE}"
 	cp "./cmake/${CMAKE_VERSION}" "${HOME}/modulefiles/cmake"
 	echo "prepend-path    PATH    ${CMAKE}/cmake-${CMAKE_VERSION}-linux-x86_64/bin" >> "${HOME}/modulefiles/cmake/${CMAKE_VERSION}"
@@ -22,11 +23,38 @@ function setup_cmake () {
 	cd "${SCRIPT_PATH}"
 }
 
+function setup_openmpi() {
+	# openmpi (8-byte integer)
+	OMPI_VERSION=$1
+	cd "${SCRIPT_PATH}"
+	OMPI_TARBALL="./openmpi/openmpi-${OMPI_VERSION}.tar.bz2"
+	OMPI_INSTALL_PREFIX="${OPENMPI}/${OMPI_VERSION}/openmpi-${OMPI_VERSION}-intel"
+	mkdir -p "${OPENMPI}/${OMPI_VERSION}"
+	tar -xf "${OMPI_TARBALL}" -C "${OPENMPI}/${OMPI_VERSION}"
+	cd "${OPENMPI}/${OMPI_VERSION}/openmpi-${OMPI_VERSION}"
+	./configure CC=icc CXX=icpc FC=ifort FCFLAGS=-i8  CFLAGS=-m64  CXXFLAGS=-m64 --enable-mpi-cxx --enable-mpi-fortran=usempi --prefix="${OMPI_INSTALL_PREFIX}"
+	make && make install && make check
+}
+
 # Unset all aliases
 \unalias -a
 
 # Setup umask
-umask 002
+umask 0022
+
+# Set the number of process
+expr $SETUP_NPROCS / 2 > /dev/null 2>&1 || SETUP_NPROCS=1 # Is $SETUP_NPROCS a number? If not, set it to 1.
+MAX_NPROCS=$(grep -c  processor /proc/cpuinfo)
+if (( $SETUP_NPROCS < 0 )); then # invalid number of processes (negative numbers, etc.)
+  echo "invalid number of processes: $SETUP_NPROCS"
+  echo "use default number of processes: 1"
+  SETUP_NPROCS=1
+elif (( $SETUP_NPROCS > $MAX_NPROCS )); then # number of processes is larger than the number of processors
+  echo "number of processors you want to use: $SETUP_NPROCS"
+  echo "number of processors you can use: $MAX_NPROCS"
+  echo "use max number of processes: $MAX_NPROCS"
+  SETUP_NPROCS=$MAX_NPROCS
+fi
 
 # Set this script's path
 # shellcheck disable=SC2046
@@ -68,13 +96,29 @@ mkdir -p "${MODULEFILES}/cmake"
 module purge
 module use --append "${MODULEFILES}"
 
-# Setup git
-setup_git && module load git/${GIT_VERSION} && git --version
+# # Setup git
+# setup_git && module load git/${GIT_VERSION} && git --version
 
-# Setup CMake
-setup_cmake && module load cmake/${CMAKE_VERSION} && cmake --version
+# # Setup CMake
+# setup_cmake && module load cmake/${CMAKE_VERSION} && cmake --version
 
 # Build OpenMPI (intel fortran)
+OPENMPI_NPROCS=$(( $SETUP_NPROCS / 2 ))
+if (( $OPENMPI_NPROCS < 1 )); then
+	# Serial build
+	echo "CMake will be built in serial mode."
+	# Build OpenMPI 3.1.0 (intel fortran)
+	setup_openmpi 3.1.0
+	# Build OpenMPI 4.1.2 (intel fortran)
+    setup_openmpi 4.1.2
+else
+	# Parallel build
+	echo "CMake will be built in parallel mode."
+	# Build OpenMPI 3.1.0 and 4.1.2 (intel fortran)
+	setup_openmpi 3.1.0 ${OPENMPI_NPROCS} | tee -a 3.1.0 & setup_openmpi 4.1.2 ${OPENMPI_NPROCS} | tee -a 4.1.2
+fi
+
+
 
 # Build DIRAC
 
